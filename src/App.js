a567872e67eb8d3246361d0fe029e3607a37846f
Enrico Appiani <eappiani@uci.edu>
2024-01-27 12:34:25 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.jpg';
 import './App.css';
+import { specificResponse } from '../open-ai-tests.mjs';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test4321
+          specificResponse: {specificResponse}
         </p>
         <a
           className="App-link"

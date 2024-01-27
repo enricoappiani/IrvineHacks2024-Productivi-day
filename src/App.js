7d36aed67ee9f7046436e7f50e8f6991e5e86a3d
Enrico Appiani <eappiani@uci.edu>
2024-01-27 13:25:21 -0800

    tst

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          out_message: {out_message}
+          test
         </p>
         <a
           className="App-link"

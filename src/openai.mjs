8c99476f12d49cb1ef1f4e79da35b01799998135
Enrico Appiani <eappiani@uci.edu>
2024-01-27 14:58:12 -0800

    ttest

--- src/openai.mjs
+++ src/openai.mjs
@@ -1,31 +1,31 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: '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'
-});
+// const openai = new OpenAI({
+//   apiKey: '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'
+// });
 
-async function main(blah) {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: blah }],
-    model: "gpt-3.5-turbo",
-  });
-  const out_message = completion.choices[0]['message']['content'];
-  const arr = out_message.split("\n")
-  const list = []
-  const info = []
-  for(const x of arr){
-    if(x.indexOf(":") >= 0){
-      list.push(x.split(":")[0])
-      info.push(x.split(":")[1])
-    }
-  }
-  console.log("\n")
-  for(const x of list){
-    console.log(x)
-  }
-  console.log("\n")
+// async function main(blah) {
+//   const completion = await openai.chat.completions.create({
+//     messages: [{ role: "system", content: blah }],
+//     model: "gpt-3.5-turbo",
+//   });
+//   const out_message = completion.choices[0]['message']['content'];
+//   const arr = out_message.split("\n")
+//   const list = []
+//   const info = []
+//   for(const x of arr){
+//     if(x.indexOf(":") >= 0){
+//       list.push(x.split(":")[0])
+//       info.push(x.split(":")[1])
+//     }
+//   }
+//   console.log("\n")
+//   for(const x of list){
+//     console.log(x)
+//   }
+//   console.log("\n")
 
-  return out_message; // Exporting the out_message variable
-}
+//   return out_message; // Exporting the out_message variable
+// }
 
 export default main; // Exporting the main function

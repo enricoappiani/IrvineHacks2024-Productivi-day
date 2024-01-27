f6feee3bdf252b77723501216954cad85a006544
arq0n <71539626+Arq0n@users.noreply.github.com>
2024-01-27 14:19:55 -0800

    General formatting with tailwind

--- src/App.js
+++ src/App.js
@@ -1,32 +1,93 @@
 import logo from './logo.jpg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div className='grid-container'>
-        <div className='grid-items'>
-          <p>daufhbsy</p>
-        </div>
-        <div className='grid-items'>
-          <p>daufhbsy</p>
-        </div>
-        <div className='grid-items'>
-          <p>daufhbsy</p>
-        </div>
-        <div className='grid-items'>
-          <p>daufhbsy</p>
-        </div>
-        <div className='grid-items'>
-          <p>daufhbsy</p>
-        </div><div className='grid-items'>
-          <p>daufhbsy</p>
-        </div>
-        
 
+      <div class="flex border-8 columns-2 gap-7 flex-auto justify-between p-10" >
+        <div class="flex border-8 grid grid-cols-2 gap-2 p-5 flex-nowrap">
+          <form> 
+            <input size="10" placeholder='1 A.M.'></input>
+          </form>  
+          <form> 
+            <input size="10" placeholder='2 A.M.'></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+          <form> 
+            <input size="10"></input>
+          </form>  
+            
+        </div>
+        <div class='flex flex-auto p-10'>
+          <h1>Output</h1>
+          <p>outpuut</p>
+        </div>
       </div>
     </div>
   );

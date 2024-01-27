0c98ce191f0832e0b76c87f9ee0effd343d65fda
arq0n <71539626+Arq0n@users.noreply.github.com>
2024-01-27 15:14:39 -0800

    Complete placeholders for text boxes

--- src/App.js
+++ src/App.js
@@ -10,83 +10,84 @@ function App() {
 
       <div class="flex border-8 columns-2 gap-7 flex-auto justify-between p-10" >
         <div class="flex border-8 grid grid-cols-2 gap-2 p-5 flex-nowrap">
+         <form> 
+            <input size="10" placeholder='12 A.M.'></input>
+          </form>  
+          <form> 
+            <input size="10" placeholder='12 P.M.'></input>
+          </form> 
           <form> 
             <input size="10" placeholder='1 A.M.'></input>
+          </form>  
+          <form> 
+            <input size="10" placeholder='1 P.M.'></input>
           </form>  
           <form> 
             <input size="10" placeholder='2 A.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='2 P.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='3 A.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='3 P.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='4 A.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='4 P.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='5 A.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='5 P.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='6 A.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='6 P.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='7 A.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='7 P.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='8 A.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='8 P.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='9 A.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='9 P.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='10 A.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='10 P.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='11 A.M.'></input>
           </form>  
           <form> 
-            <input size="10"></input>
+            <input size="10" placeholder='11 P.M.'></input>
           </form>  
-          <form> 
-            <input size="10"></input>
-          </form>  
-          <form> 
-            <input size="10"></input>
-          </form>  
-          <form> 
-            <input size="10"></input>
-          </form>  
+
             
         </div>
-        <div class='flex flex-auto p-10'>
+        <div class='flex p-10'>
           <h1>Output</h1>
-          <p>outpuut</p>
+          <p class='border-8'>outpuut</p>
         </div>
       </div>
     </div>

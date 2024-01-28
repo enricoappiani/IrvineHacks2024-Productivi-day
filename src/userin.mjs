fcac8bd5aa7b8811220df55e6c4be63bb073ac5e
Enrico Appiani <eappiani@uci.edu>
2024-01-27 17:45:06 -0800

    m

--- src/userin.mjs
+++ src/userin.mjs
@@ -1,5 +1,5 @@
 import main from './openai.mjs'
-export default function schedule_changer(activities_list){
+export default async function schedule_changer(activities_list){
     const times_list = ['12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM'];
 
     for (let i = 0; i < times_list.length; i++) {

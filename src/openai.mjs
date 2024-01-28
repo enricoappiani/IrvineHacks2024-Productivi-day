eef87d5887f2c663729af0dc7d4e7264fa09b6a5
Enrico Appiani <eappiani@uci.edu>
2024-01-27 17:03:47 -0800

    j

--- src/openai.mjs
+++ src/openai.mjs
@@ -20,13 +20,9 @@ export default async function main(blah) {
       info.push(x.split(":")[1])
     }
   }
-  /*
-  console.log("\n")
-  for(const x of list){
-    console.log(x)
-  }
-  console.log("\n")
-  */
+ 
   const all_info = list.join("\n")
+  console.log(all_info)
   return all_info;
 }
+main("My schedule is: wake up 8 am, play league to 12pm, go to class to 1pm, eat lunch which was Jamaican Jerk, do homework from 2pm to 5pm, eat dinner which was Jamaican Jerk (I love Jamaican Jerk), then play league for 5 hours, then homework from 11pm to 3 am, sleep at 3 am. What are some bad habits of my schedule?")

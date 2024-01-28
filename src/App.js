0bd71eb5c461b7f7bcff2392d8911aa83eef9057
Enrico Appiani <eappiani@uci.edu>
2024-01-27 16:50:26 -0800

    please almighty lord

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.jpg';
 import './App.css';
-import out_message from './openai.mjs';
+import main from './openai.mjs'
 
 function App() {
+  const [msg, settMsg ] = useState("")
+  useEffect(() => {
+    const lol = main("My schedule is: wake up 8 am, play league to 12pm, go to class to 1pm, eat lunch which was Jamaican Jerk, do homework from 2pm to 5pm, eat dinner which was Jamaican Jerk (I love Jamaican Jerk), then play league for 5 hours, then homework from 11pm to 3 am, sleep at 3 am. What are some bad habits of my schedule?")
+    lol.then((res) => settMsg(res))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test
+          {msg}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
--- src/openai.mjs
+++ src/openai.mjs
@@ -1,13 +1,15 @@
 import OpenAI from "openai";
 
+export default async function main(word){
 const openai = new OpenAI({
    apiKey: process.env.REACT_APP_OPENAI_API_KEY, dangerouslyAllowBrowser: true
  });
 
- const completion = await openai.chat.completions.create({
-   messages: [{ role: "system", content: "You are a helpful assistant." }],
-   model: "gpt-3.5-turbo",
- });
- const out_message = (completion.choices[0]['message']['content']);
-
-export default out_message;
+const completion = await openai.chat.completions.create({
+  messages: [{ role: "system", content: word}],
+  model: "gpt-3.5-turbo",
+});
+const out_message = completion.choices[0]['message']['content'];
+return out_message;
+}
+ 

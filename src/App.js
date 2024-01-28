0a38cb40022678201aecd0accecd30d9d0e1512f
arq0n <71539626+Arq0n@users.noreply.github.com>
2024-01-27 21:20:05 -0800

    Add buttons after each hour after merge

--- src/App.js
+++ src/App.js
@@ -22,14 +22,36 @@ function App() {
   return (
     
     <div className="App">
-      <div class="h-14 bg-gradient-to-r from-cyan-500 to-blue-500"></div>
+      <div class="h-14 bg-gradient-to-r rounded-4xl from-cyan-500 to-blue-500"></div>
       <h2 class="text-8xl font-extrabold dark:text-white p-5">☀️Productivi-Day☀️</h2>
       
-      <div class="flex border-8 columns-2 gap-7 rounded-3xl flex-auto justify-between p-10 border-indigo-50	" >
+      <div class="flex border-8 columns-2 gap-7 rounded-4xl flex-auto justify-between p-10 border-indigo-50	" >
       <p class="my-4 bold text-5xl font-bold text-gray-300">Activity log:</p>
         <div class="w-full flex border-8 grid grid-cols-2 gap-2 p-5 rounded-3xl flex-balance text-gray-600 border-indigo-50	">
          <Textbox name='12am' time="12 A.M." handleChange={handleActivityChange} activity={activity}/>
          <Textbox name='12pm' time="12 P.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='1am' time="1 A.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='1pm' time="1 P.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='2am' time="2 A.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='2pm' time="2 P.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='3am' time="3 A.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='3pm' time="3 P.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='4am' time="4 A.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='4pm' time="4 P.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='5am' time="5 A.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='5pm' time="5 P.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='6am' time="6 A.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='6pm' time="6 P.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='7am' time="7 A.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='7pm' time="7 P.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='8am' time="8 A.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='8pm' time="8 P.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='9am' time="9 A.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='9pm' time="9 P.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='10am' time="10 A.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='10pm' time="10 P.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='11am' time="11 A.M." handleChange={handleActivityChange} activity={activity}/>
+         <Textbox name='11pm' time="11 P.M." handleChange={handleActivityChange} activity={activity}/>
         </div>
         <div class='p-10'>
           <h1 class='font-bold'>Output</h1>
--- src/components/Textbox.jsx
+++ src/components/Textbox.jsx
@@ -1,6 +1,6 @@
 const Textbox = ({name, time, handleChange, activity}) => {
     return (
-        <form class="bg-white shadow-md rounded px-2 pt-6 pb-8 mb-4">
+        <form class="bg-white shadow-md rounded-4xl px-2 pt-6 pb-8 mb-4">
             <input size="15" name={name} placeholder={time} onChange={handleChange} value={activity.time}></input>
         </form>
     )

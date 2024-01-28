4a75a27cb66511bea3f2a96e0f3ab1d281a30e3a
Enrico Appiani <eappiani@uci.edu>
2024-01-27 16:57:59 -0800

    donke

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import main from './openai.mjs'
 
 function App() {
-  const [msg, settMsg ] = useState("")
+  const [msg, setMsg ] = useState("")
   useEffect(() => {
     const lol = main("My schedule is: wake up 8 am, play league to 12pm, go to class to 1pm, eat lunch which was Jamaican Jerk, do homework from 2pm to 5pm, eat dinner which was Jamaican Jerk (I love Jamaican Jerk), then play league for 5 hours, then homework from 11pm to 3 am, sleep at 3 am. What are some bad habits of my schedule?")
-    lol.then((res) => settMsg(res))
+    lol.then((res) => setMsg(res))
   }, [])
 
   return (
--- src/openai.mjs
+++ src/openai.mjs
@@ -1,15 +1,32 @@
 import OpenAI from "openai";
+//require('dotenv').config()
 
-export default async function main(word){
 const openai = new OpenAI({
    apiKey: process.env.REACT_APP_OPENAI_API_KEY, dangerouslyAllowBrowser: true
  });
 
-const completion = await openai.chat.completions.create({
-  messages: [{ role: "system", content: word}],
-  model: "gpt-3.5-turbo",
-});
-const out_message = completion.choices[0]['message']['content'];
-return out_message;
+export default async function main(blah) {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: blah }],
+    model: "gpt-3.5-turbo",
+  });
+  const out_message = completion.choices[0]['message']['content'];
+  const arr = out_message.split("\n")
+  const list = []
+  const info = []
+  for(const x of arr){
+    if(x.indexOf(":") >= 0){
+      list.push(x.split(":")[0])
+      info.push(x.split(":")[1])
+    }
+  }
+  /*
+  console.log("\n")
+  for(const x of list){
+    console.log(x)
+  }
+  console.log("\n")
+  */
+  const all_info = list.join("\n")
+  return all_info;
 }
- 

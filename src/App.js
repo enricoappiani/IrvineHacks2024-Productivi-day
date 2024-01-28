231f278977cf1f787d9edfa5e215583dddc53b2f
arq0n <71539626+Arq0n@users.noreply.github.com>
2024-01-27 16:35:29 -0800

    Remove framer motion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.jpg';
 import './App.css';
 import { useState, useEffect } from 'react'
-import { motion } from 'framer-motion'
 
 function App() {
   
@@ -13,77 +12,77 @@ function App() {
       <div class="flex border-8 columns-2 gap-7 flex-auto justify-between p-10 border-sky-400" >
       <p class="my-4 text-lg text-gray-300">Activity log:</p>
         <div class="w-full flex border-8 grid grid-cols-2 gap-2 p-5 flex-nowrap text-gray-600 border-sky-400	">
-         <form class="bg-white shadow-md rounded px-8 pt-6 pb-6 mb-4"> 
-            <input size="20" placeholder='12 A.M.'></input>
+         <form class="bg-white shadow-md rounded px-8 pt-3 pb-3 mb-4"> 
+            <input size="15" placeholder='12 A.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='12 P.M.'></input>
+            <input size="15" placeholder='12 P.M.'></input>
           </form> 
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" >
-            <input size="10" placeholder='1 A.M.'></input>
+            <input size="15" placeholder='1 A.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='1 P.M.'></input>
+            <input size="15" placeholder='1 P.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='2 A.M.'></input>
+            <input size="15" placeholder='2 A.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='2 P.M.'></input>
+            <input size="15" placeholder='2 P.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='3 A.M.'></input>
+            <input size="15" placeholder='3 A.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='3 P.M.'></input>
+            <input size="15" placeholder='3 P.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">  
-            <input size="10" placeholder='4 A.M.'></input>
+            <input size="15" placeholder='4 A.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='4 P.M.'></input>
+            <input size="15" placeholder='4 P.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='5 A.M.'></input>
+            <input size="15" placeholder='5 A.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='5 P.M.'></input>
+            <input size="15" placeholder='5 P.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='6 A.M.'></input>
+            <input size="15" placeholder='6 A.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='6 P.M.'></input>
+            <input size="15" placeholder='6 P.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='7 A.M.'></input>
+            <input size="15" placeholder='7 A.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='7 P.M.'></input>
+            <input size="15" placeholder='7 P.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='8 A.M.'></input>
+            <input size="15" placeholder='8 A.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='8 P.M.'></input>
+            <input size="15" placeholder='8 P.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='9 A.M.'></input>
+            <input size="15" placeholder='9 A.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='9 P.M.'></input>
+            <input size="15" placeholder='9 P.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='10 A.M.'></input>
+            <input size="15" placeholder='10 A.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='10 P.M.'></input>
+            <input size="15" placeholder='10 P.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">  
-            <input size="10" placeholder='11 A.M.'></input>
+            <input size="15" placeholder='11 A.M.'></input>
           </form>  
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-            <input size="10" placeholder='11 P.M.'></input>
+            <input size="15" placeholder='11 P.M.'></input>
           </form >  
         </div>
         <div class='p-10'>
